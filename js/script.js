9412ba3b588f472fd74ad94a496c7f5e2e4c536e
Henrique Melanda <henrique.melanda@C02Y256CJG5H.local>
2019-06-30 12:07:44 -0300

    Fixed the toggle of changing the color of the site that was not facing the dark mode after switching to litght mode

--- js/script.js
+++ js/script.js
@@ -3,7 +3,9 @@
   const nigthMode = document.querySelector('[data-js="lamp"]');
   
   nigthMode.addEventListener('click', function(){
+    
     this.classList.toggle('active');
+    
     if (!this.classList.contains('active')) {
       document.documentElement.style.setProperty('--color-black', '#FFFFFF');
       document.documentElement.style.setProperty('--color-white', '#000000');
@@ -12,7 +14,17 @@
       document.documentElement.style.setProperty('--color-yellow', '#7d6004');
       document.documentElement.style.setProperty('--color-green', '#096701');
       document.documentElement.style.setProperty('--color-footer', '#e8e8e8');
+      return;
     }
+    
+    document.documentElement.style.setProperty('--color-black', '#000000');
+    document.documentElement.style.setProperty('--color-white', '#FFFFFF');
+    document.documentElement.style.setProperty('--color-lightGray', '#b3b3b3');
+    document.documentElement.style.setProperty('--color-lightBlue', '#58A9f3');
+    document.documentElement.style.setProperty('--color-yellow', '#FBC54C');
+    document.documentElement.style.setProperty('--color-green', '#7BE473');
+    document.documentElement.style.setProperty('--color-footer', '#171616');
+    
   });
 
 })();
